3ff4648271686607e79ff2352e6222a602225cb6
yuuya-saitou <ktkstyyshaba@gmail.com>
2020-09-05 00:08:41 +0900

    gem等の環境設定

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_04_14_072724) do
 
-  create_table "plans", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "plans", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "plan"
     t.date "date"
     t.datetime "created_at", precision: 6, null: false
